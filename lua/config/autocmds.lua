43e21f59655cc305e7a3caaeaa9c10922f9184ab
Trevor Arjeski <tmarjeski@gmail.com>
2024-01-12 21:15:51 +0300

    autocmd for text file wrap and spell

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -19,3 +19,14 @@ vim.api.nvim_create_autocmd({ "VimResized" }, {
     vim.cmd("tabnext " .. current_tab)
   end,
 })
+
+-- wrap and check for spell in text filetypes
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup("wrap_spell"),
+  pattern = { "gitcommit", "markdown" },
+  callback = function()
+    vim.opt_local.wrap = true
+    vim.opt_local.textwidth = 80
+    vim.opt_local.spell = true
+  end,
+})
