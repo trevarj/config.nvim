0b07faba488b50ac523dad8116b6e860a656d863
Trevor Arjeski <tmarjeski@gmail.com>
2024-01-25 17:38:47 +0300

    add crates cmp to rust setup

--- lua/plugins/lsp/rust.lua
+++ lua/plugins/lsp/rust.lua
@@ -43,4 +43,23 @@ return {
       }
     end,
   },
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      {
+        "Saecki/crates.nvim",
+        event = { "BufRead Cargo.toml" },
+        opts = {
+          src = {
+            cmp = { enabled = true },
+          },
+        },
+      },
+    },
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      opts.sources = opts.sources or {}
+      table.insert(opts.sources, { name = "crates" })
+    end,
+  },
 }
