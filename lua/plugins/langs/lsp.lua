ddb2ef12d2bb6c6a5878b1953419cf1bc4024da3
Trevor Arjeski <tmarjeski@gmail.com>
2024-03-14 14:58:47 +0300

    lsp cleanup, remove mason

--- lua/plugins/langs/lsp.lua
+++ lua/plugins/langs/lsp.lua
@@ -1,3 +1,4 @@
+---@diagnostic disable: missing-fields
 return {
   {
     "neovim/nvim-lspconfig",
@@ -5,16 +6,11 @@ return {
     dependencies = {
       { "folke/neoconf.nvim", config = false },
       { "folke/neodev.nvim", opts = {} },
-      "mason.nvim",
-      "williamboman/mason-lspconfig.nvim",
       { "j-hui/fidget.nvim", opts = {} },
-      "nvim-telescope/telescope.nvim",
     },
-    ---@class PluginLspOpts
     opts = function()
       local icons = require("config.icons")
       return {
-        -- options for vim.diagnostic.config()
         diagnostics = {
           underline = true,
           update_in_insert = false,
@@ -46,14 +42,8 @@ return {
         },
         -- LSP Server Settings
         ---@type lspconfig.options
-        ---@diagnostic disable-next-line: missing-fields
         servers = {
           lua_ls = {
-            -- mason = false, -- set to false if you don't want this server to be installed with mason
-            -- Use this to add any additional keymaps
-            -- for specific lsp servers
-            ---@type LazyKeysSpec[]
-            -- keys = {},
             settings = {
               Lua = {
                 workspace = {
@@ -65,12 +55,15 @@ return {
               },
             },
           },
+          bashls = {},
+          taplo = {},
+          yamlls = {},
+          gopls = {},
         },
         ---@type table<string, fun(server:string, opts:_.lspconfig.options):boolean?>
         setup = {},
       }
     end,
-    ---@param opts PluginLspOpts
     config = function(_, opts)
       local plugin = require("lazy.core.config").spec.plugins["neoconf.nvim"]
       require("neoconf").setup(require("lazy.core.plugin").values(plugin, "opts", false))
@@ -102,90 +95,23 @@ return {
         opts.capabilities or {}
       )
 
-      local function setup(server)
-        local server_opts = vim.tbl_deep_extend("force", {
-          capabilities = vim.deepcopy(capabilities),
-        }, servers[server] or {})
-
-        if opts.setup[server] then
-          if opts.setup[server](server, server_opts) then
-            return
-          end
-        elseif opts.setup["*"] then
-          if opts.setup["*"](server, server_opts) then
-            return
-          end
-        end
-        require("lspconfig")[server].setup(server_opts)
-      end
-
-      -- get all the servers that are available through mason-lspconfig
-      local have_mason, mlsp = pcall(require, "mason-lspconfig")
-      local all_mslp_servers = {}
-      if have_mason then
-        all_mslp_servers = vim.tbl_keys(require("mason-lspconfig.mappings.server").lspconfig_to_package)
-      end
-
-      local ensure_installed = {} ---@type string[]
       for server, server_opts in pairs(servers) do
         if server_opts then
-          server_opts = server_opts == true and {} or server_opts
-          -- run manual setup if mason=false or if this is a server that cannot be installed with mason-lspconfig
-          if server_opts.mason == false or not vim.tbl_contains(all_mslp_servers, server) then
-            setup(server)
-          else
-            ensure_installed[#ensure_installed + 1] = server
-          end
-        end
-      end
+          server_opts = vim.tbl_deep_extend("force", {
+            capabilities = vim.deepcopy(capabilities),
+          }, server_opts or {})
 
-      if have_mason then
-        mlsp.setup({ ensure_installed = ensure_installed, handlers = { setup } })
-      end
-    end,
-  },
-  {
-    "williamboman/mason.nvim",
-    cmd = "Mason",
-    keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } },
-    build = ":MasonUpdate",
-    opts = {
-      ensure_installed = {
-        "lua-language-server",
-        "stylua",
-        "shfmt",
-        -- "flake8",
-      },
-      ui = {
-        border = "rounded",
-        height = 0.8,
-      },
-    },
-    ---@param opts MasonSettings | {ensure_installed: string[]}
-    config = function(_, opts)
-      require("mason").setup(opts)
-      local mr = require("mason-registry")
-      mr:on("package:install:success", function()
-        vim.defer_fn(function()
-          -- trigger FileType event to possibly load this newly installed LSP server
-          require("lazy.core.handler.event").trigger({
-            event = "FileType",
-            buf = vim.api.nvim_get_current_buf(),
-          })
-        end, 100)
-      end)
-      local function ensure_installed()
-        for _, tool in ipairs(opts.ensure_installed) do
-          local p = mr.get_package(tool)
-          if not p:is_installed() then
-            p:install()
+          if opts.setup[server] then
+            if opts.setup[server](server, server_opts) then
+              return
+            end
+          elseif opts.setup["*"] then
+            if opts.setup["*"](server, server_opts) then
+              return
+            end
           end
+          require("lspconfig")[server].setup(server_opts)
         end
-      end
-      if mr.refresh then
-        mr.refresh(ensure_installed)
-      else
-        ensure_installed()
       end
     end,
   },
